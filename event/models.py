f2018fc56c660a891b5bf31f9e304f2665e801d7
Earl Tacogdoy <earltacogdoy@gmail.com>
2024-08-15 01:15:16 +0800

    Tuple Update

--- event/models.py
+++ event/models.py
@@ -1,22 +1,24 @@
 import datetime
 
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 from django.contrib import admin
 
 # Create your models here.
 class Event(models.Model):
-    class CountryCode(models.TextChoices):
-        PH = "PH", "+63"
-        AU = "AU", "+61"
-        NZ = "NZ", "+64"
+    CountryCodes = (
+        ("PH", _("+63")),
+        ("AU", _("+61")),
+        ("NZ", _("+64"))
+    )
     
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=255)
     image = models.ImageField(upload_to="static/images")
     email = models.EmailField()
     contact = models.CharField(max_length=25)
-    dial_code = models.CharField(max_length=3, choices=CountryCode, default=CountryCode.PH)
+    dial_code = models.CharField(max_length=3, choices=CountryCodes, default=CountryCodes.PH)
     date_start = models.DateField("date start")
     date_end = models.DateField("date end")
     date_created = models.DateTimeField("date created", auto_now=True)
